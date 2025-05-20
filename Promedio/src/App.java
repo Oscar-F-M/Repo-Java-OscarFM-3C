72585716a5fa8c2c9f178c51474ea028f58f7173
Oscar-F-M <20243ds081@utez.edu.mx>
2025-05-20 10:01:25 -0600

    se agrega script de promedio

---
+++ Promedio/src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+          String numero1;
+        String numero2;
+        String numero3;
+        numero1 = JOptionPane.showInputDialog("Ingrese el primer numero");
+        numero2 =JOptionPane.showInputDialog("Ingresa el segundo numero");
+        numero3 = JOptionPane.showInputDialog("Ingresa el tercer numero");
+
+    
+        System.out.println("El promedio de los tres numeros es:: " + (Double.parseDouble(numero1) + Double.parseDouble(numero2) + Double.parseDouble(numero3))/3);
+    }
+}
