7ba97b6648a5260e7e34cad82b1c6ea9314a6d5c
Oscar-F-M <20243ds081@utez.edu.mx>
2025-06-03 21:55:59 -0600

    Se agrega el primer trabajo con javaFX

---
+++ demoOscarJavaFX/src/main/java/com/example/demooscarjavafx/HelloApplication.java
@@ -0,0 +1,35 @@
+package com.example.holamundojavafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Label label = new Label("Hola Profe");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 320, 240);
+
+
+        stage.setTitle("Mi primer app en Java FX");
+        Image image = new Image(getClass().getResourceAsStream("Kirito.png"));
+        stage.getIcons().add(image);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
